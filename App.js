8c63d7f86a7179fbb23abb98acdc347e2edecd7c
amarjeetk123 <ak123del@gmail.com>
2022-10-19 18:39:10 +0530

    Adding a readme file

--- App.js
+++ App.js
@@ -15,41 +15,32 @@ generateBtn.addEventListener("click", () => {
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${user_input}`;
     qrImg.addEventListener("load", () => {
         main.classList.add("active");
-        // main.classList.add("change_btn")
+        main.classList.add("change_btn")
     });
-let saveUrl = qrImg.src
- creatSavebtn(saveUrl)
+
+   
+ 
 });
 
-
-
 // code for Downloading the QR code
-const creatSavebtn = (saveUrl) =>{
-// download_btn.href = qrImg.src
-// link.download = 'qrcode';
-// download_btn.innerHTML = "sskksk"
-// console.log(download_btn.href)
-const link = document.createElement('a');
-link.id = 'save-link';
-link.classList.add("my")
-  
-link.href = saveUrl;
-link.download = 'qrcode';
-link.innerHTML = 'Save Image';
-document.querySelector("#gen").appendChild(link);
-}
+
+download_btn.addEventListener("click" , ()=>{
+    // const canvas = document.createElement("canvas")
+    // canvas.src =  qrImg.src
+    // document.getElementById("gen").appendChild(canvas)
+          var a = document.createElement('a');
+        //   a.href = "./image/Project.png" 
+        //   a.href = qrImg.src
+          a.href = "qrImg.src"
+          a.download = "a.png"
+        
+          document.body.appendChild(a);
+          a.click();
+
+          document.body.removeChild(a);
+          })
 
 
-// const createSaveBtn = () => {
-//     const link = document.createElement('a');
-//     link.id = 'save-link';
-//     link.classList =
-//       'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
-//     link.href = qrImg.src;
-//     link.download = 'qrcode';
-//     link.innerHTML = 'Save Image';
-//     document.getElementById('generated').appendChild(link);
-//   };
 
 qrInput.addEventListener("input", () => {
     if(qrInput.value.trim() == "") {
@@ -57,3 +48,18 @@ qrInput.addEventListener("input", () => {
         main.classList.remove("active");
     }
 });
+
+
+
+// var link = document.getElementById("download");
+// link.addEventListener("click", setUpDownload);
+// function setUpDownload() {
+//     // Find the image inside the #qrcode div
+//     var image = document.getElementById("qrcode").getElementsByTagName("img");
+
+//     // Get the src attribute of the image, which is the data-encoded QR code
+//     var qr = image[0].src;
+// // console.log(qr)
+//     // Copy that to the download link
+//     link.src = qr;
+// }
---
+++ readme.md
@@ -0,0 +1,22 @@
+# QR Code Generator
+ ### I have made QR Code Generator with the help of HTML, CSS, and JavaScript.
+
+
+![](https://img.shields.io/badge/Project-10-pink)
+ ![](https://img.shields.io/badge/JS-Project-yellow)
+![](https://img.shields.io/badge/HTML-5-orange)
+ ![](https://img.shields.io/badge/CSS-3-blue)
+ ![](https://img.shields.io/badge/LCO-Hitesh%20Sir-p)
+
+Live Link:- [Click here!](https://amarjeet-qr-code-generator.netlify.app/)
+
+Picture of my project:-
+![](./image/image.png)
+
+## Technology Used:-
+  - ` HTML ` 
+  - ` CSS ` 
+  - ` JS ` 
+
+
+### It took 2 hours to complete.
